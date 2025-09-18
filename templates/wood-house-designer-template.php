5356db9bcb7af795854f723766b31516f7a88f97
cosemurciano <cose.murciano@gmail.com>
2025-09-18 12:55:58 +0200

    Refine full-screen template and cottage tooling

--- templates/wood-house-designer-template.php
+++ templates/wood-house-designer-template.php
@@ -7,16 +7,41 @@
 
 defined( 'ABSPATH' ) || exit;
 
-get_header();
-?>
-<main id="primary" class="site-main wood-house-designer-template">
-    <?php
-    while ( have_posts() ) {
-        the_post();
-        the_content();
-        echo do_shortcode( '[wood_house_designer]' );
-    }
-    ?>
-</main>
-<?php
-get_footer();
+?><!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+    <head>
+        <meta charset="<?php bloginfo( 'charset' ); ?>" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <?php wp_head(); ?>
+    </head>
+    <body <?php body_class( 'wood-house-designer-fullscreen' ); ?>>
+        <?php
+        if ( function_exists( 'wp_body_open' ) ) {
+            wp_body_open();
+        }
+        ?>
+        <div class="wood-house-designer-fullscreen__container">
+            <?php
+            while ( have_posts() ) {
+                the_post();
+                $raw_content   = get_the_content();
+                $has_shortcode = has_shortcode( $raw_content, 'wood_house_designer' );
+                ?>
+                <div class="wood-house-designer-fullscreen__content">
+                    <?php
+                    if ( ! empty( $raw_content ) ) {
+                        echo apply_filters( 'the_content', $raw_content ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+                    }
+
+                    if ( ! $has_shortcode ) {
+                        echo do_shortcode( '[wood_house_designer]' ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+                    }
+                    ?>
+                </div>
+                <?php
+            }
+            ?>
+        </div>
+        <?php wp_footer(); ?>
+    </body>
+</html>
